ab91ed0164d6ace9ec09181447e36c7068a3a857
Emma Gerigscott <elgerig@gmail.com>
2023-04-14 09:32:39 -0700

    pass test 7 green phase mercuryYears method returns correct year when called and update property mercuryAge to use method

--- src/js/spaceAge.js
+++ src/js/spaceAge.js
@@ -1,13 +1,13 @@
 export default class SpaceAge {
   constructor(earthYears) {
     this.earthYears = earthYears;
-    this.mercuryAge = parseFloat((this.earthYears / 0.24).toFixed(2));
+    this.mercuryAge = this.mercuryYears(earthYears);
     this.venusAge = parseFloat((this.earthYears / 0.62).toFixed(2));
     this.marsAge = parseFloat((this.earthYears / 1.88).toFixed(2));
     this.jupiterAge = parseFloat((this.earthYears / 11.86).toFixed(2));
   }
 
   mercuryYears(years) {
-    return 0
+    return parseFloat((years / 0.24).toFixed(2));
   }
 }
