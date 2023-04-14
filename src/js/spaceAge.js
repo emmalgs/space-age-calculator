c9ed11e1d641934f600a0ba7c21d7c586e6ddaca
Emma Gerigscott <elgerig@gmail.com>
2023-04-14 09:13:19 -0700

    pass test 5 green phase

--- src/js/spaceAge.js
+++ src/js/spaceAge.js
@@ -1,6 +1,9 @@
 export default class SpaceAge {
   constructor(earthYears) {
     this.earthYears = earthYears;
+    this.mercuryAge = parseFloat((this.earthYears / 0.24).toFixed(2));
+    this.venusAge = parseFloat((this.earthYears / 0.62).toFixed(2));
     this.marsAge = parseFloat((this.earthYears / 1.88).toFixed(2));
+    this.jupiterAge = parseFloat((this.earthYears / 11.86).toFixed(2));
   }
 }
