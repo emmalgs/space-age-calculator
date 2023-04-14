4a47bcc08dd0c192af6cb9a74a4c4c1d4a73aa64
Emma Gerigscott <elgerig@gmail.com>
2023-04-14 11:43:20 -0700

    add forms for years since and years until birthday

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import './css/styles.css';
 import SpaceAge from "./js/spaceAge";
 
-function handleAgeSubmit(e) {
-  e.preventDefault();
-  const earthAge = document.getElementById("earth-age").value;
-  let spaceAge = new SpaceAge(earthAge);
-  displayPlanets(spaceAge);
-}
-
 function displayPlanets(age) {
   const mercuryDiv = document.getElementById("mercury");
   const venusDiv = document.getElementById("venus");
@@ -21,5 +14,18 @@ function displayPlanets(age) {
   marsDiv.querySelector("p").innerText = age.marsAge;
   jupiterDiv.querySelector("p").innerText = age.jupiterAge;
 }
+
+function handleAgeSubmit(e) {
+  e.preventDefault();
+  const earthAge = document.getElementById("earth-age").value;
+  let spaceAge = new SpaceAge(earthAge);
+  let forms = document.querySelectorAll("div.age-form");
+  forms.forEach((form) => {
+    form.classList.remove("hidden")
+  });
+  displayPlanets(spaceAge);
+}
+
+
 
 document.getElementById("earth-age-form").addEventListener("submit", handleAgeSubmit);
