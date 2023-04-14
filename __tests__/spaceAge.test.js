77ef8e2fbda3853694148b0ff13afa15e692303b
Emma Gerigscott <elgerig@gmail.com>
2023-04-14 08:55:31 -0700

    red phase test 3

--- __tests__/spaceAge.test.js
+++ __tests__/spaceAge.test.js
@@ -1,13 +1,24 @@
 import SpaceAge from "../src/js/spaceAge";
 
 describe('SpaceAge', () => {
+  let spaceAge;
+  let earthYears;
+
+  beforeEach(() => {
+    earthYears = 32;
+    spaceAge = new SpaceAge(earthYears);
+  })
+
   test('should create an instance of an object when called', () => {
-    let spaceAge = new SpaceAge()
     expect(typeof spaceAge).toEqual('object');
   });
 
   test('should take an earthYears argument and return when property earthYears is called', () => {
-    let spaceAge = new SpaceAge(32);
     expect(spaceAge.earthYears).toEqual(32);
+  });
+  
+    test('should have a method to calculate mars years and return a number', () => {
+      let marsAge = spaceAge.marsAge();
+      expect(marsAge).toEqual(Number(marsAge));
   });
 });
