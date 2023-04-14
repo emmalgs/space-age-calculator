bca42719a0efba3a02bc5d836c7dc61af1a2aacc
Emma Gerigscott <elgerig@gmail.com>
2023-04-14 10:05:46 -0700

    display age of each planet on submit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,21 @@ function handleAgeSubmit(e) {
   e.preventDefault();
   const earthAge = document.getElementById("earth-age").value;
   let spaceAge = new SpaceAge(earthAge);
-  console.log(spaceAge);
+  displayPlanets(spaceAge);
 }
 
-document.getElementById("earth-age-form").addEventListener("submit", handleAgeSubmit)
+function displayPlanets(age) {
+  const mercuryDiv = document.getElementById("mercury");
+  const venusDiv = document.getElementById("venus");
+  const earthDiv = document.getElementById("earth");
+  const marsDiv = document.getElementById("mars");
+  const jupiterDiv = document.getElementById("jupiter");
+
+  mercuryDiv.innerText = age.mercuryAge;
+  venusDiv.innerText = age.venusAge;
+  earthDiv.innerText = age.earthYears;
+  marsDiv.innerText = age.marsAge;
+  jupiterDiv.innerText = age.jupiterAge;
+}
+
+document.getElementById("earth-age-form").addEventListener("submit", handleAgeSubmit);
